3987e7c9f451155c72c784e67a68bb6c3ff75031
Abdul Ahad <mrad232000@gmail.com>
2024-08-02 17:31:02 +0500

    Assignment 07

---
+++ Assignment#07/main.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+void main() {
+  List<int> myList = [];
+
+  //Method - 01
+  myList.add(50);
+  print(myList);
+
+  //Method - 02
+  myList.addAll([100, 60, 10, 5, 7, 50]);
+  print(myList);
+
+  //Method - 03
+  print(myList.contains(50));
+
+  //Method - 04
+  myList.remove(50);
+  print(myList);
+
+  //Method - 05
+  print(myList.first);
+
+  //Method - 06
+  print(myList.last);
+
+  //Method - 07
+  print(myList.reversed.toList());
+
+  //Method - 08
+  myList.sort();
+  print(myList);
+
+  //Method - 09
+  print(myList.length);
+
+  //Method - 10
+  print(myList.getRange(0, 2));
+}
