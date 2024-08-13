39a4a9aadca1bbd779ceece5a6d6c9fcc22b9e2e
Abdul Ahad <abdulahad.ad23@gmail.com>
2024-08-13 19:40:33 +0500

    Marks Sheet Function Practice

--- Practice/main.dart
+++ Practice/main.dart
@@ -1,28 +1,22 @@
 import "dart:io";
 
 void main() {
-  for (int i = 10; i >= 1; i--) {
-    print("5 x $i = ${5 * i}");
-  }
+  displayMarksSheet("Abdul Ahad", 80, 73, 79, 95);
+}
 
-  List list = ['Abdul', 'Ahad', 'Saba'];
-  for (int i = 0; i < list.length; i++) {
-    stdout.write(list[i] + ", ");
-  }
-  print("");
-  for (var name in list) {
-    stdout.write(name + ", ");
-  }
-
-  print("");
-  int multiplier = 1;
-  while (multiplier != 11) {
-    print("5 x $multiplier = ${5 * multiplier}");
-    multiplier++;
-  }
-
-  int i = 0;
-  do {
-    print("Hello - $i");
-  } while (i < 0);
+displayMarksSheet(name, sub1, sub2, sub3, sub4) {
+  int totalMarks = 400;
+  int obtainedMarks = sub1 + sub2 + sub3 + sub4;
+  print("------------------------");
+  print("Name : $name");
+  print("------------------------");
+  print("Subject 1 Marks : $sub1");
+  print("Subject 2 Marks : $sub2");
+  print("Subject 3 Marks : $sub3");
+  print("Subject 4 Marks : $sub4");
+  print("------------------------");
+  print("Total Marks : $totalMarks");
+  print("Obtained Marks : $obtainedMarks");
+  print("Percentage : ${(obtainedMarks / totalMarks) * 100}");
+  print("------------------------");
 }
